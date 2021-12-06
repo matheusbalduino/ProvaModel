85bce590a9369b992de90e92bdcf35d84cbcd07e
Matheus Balduino <matheus_mgb@hotmail.com>
2021-12-06 20:30:27 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaModel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProvaModel.MvcApplication" Language="C#" %>
---
+++ ProvaModel/Models/Fornecedor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProvaModel.Models
+{
+    public class Fornecedor
+    {
+        public long FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+    public class Produto
+    {
+        public long ProdutoId { get; set; }
+        public long FornecedorId { get; set; } //Foreign Key de fornecedor
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public Fornecedor Fornecedor { get; set; }   
+    
+    }
+}
